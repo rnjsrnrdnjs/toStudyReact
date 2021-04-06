e20e4322979a6fcc315b632523358c4b46cedeed
rnjsrnrdnjs <090tree@gmail.com>
2021-04-06 14:33:43 +0000

    todoApp

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+
+const App=()=>{
+	return (<TodoTemplate>
+		<TodoInsert/>
+	</TodoTemplate>
+	);
+};
+
+export default App;
---
+++ todo-app/src/components/TodoInsert.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {MdAdd} from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert=()=>{
+	return (
+		<form calssName="TodoInsert">
+		<input placeholder="할 일을 입력하세요"/>
+		<button type="submit"><MdAdd/></button>
+		</form>
+	);
+};
+export default TodoInsert;
